ceae5ee398f3dd4c9fd2404ea72c70029fe88a75
HUNG CHU <cphung1997@gmail.com>
2020-12-19 16:34:09 -0600

    add more comment and more

--- section2_DOM/Event-Bubbling-Delegation.js
+++ section2_DOM/Event-Bubbling-Delegation.js
@@ -2,10 +2,13 @@
  Event bubbling 
  1. bubbling up of the event through the DOM from the child element 
  to the parent
+ 2. When an event happens on an element, it first runs the handlers on it, then on its parent, then all the way up on other ancestors. 
+ 3. For example, if child element used the same event listern like parent like click event 
 
  Event Delegation
  1. bubbling down of the event through the DOM from the parent element 
  to the child -> used after the html page is done loading
+ 2. The idea is that if we have a lot of elements handled in a similar way, then instead of assigning a handler to each of them – we put a single handler on their common ancestor.
  */
 
 //  get the card-title class name
